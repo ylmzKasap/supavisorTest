1d11031af9792430ae41a4bdbad4e2645cb5a480
Yılmaz Kasap <ylmzkasap@outlook.com>
2024-01-11 17:51:33 +0300

    increase requests

--- message.js
+++ message.js
@@ -2,7 +2,7 @@ module.exports = async function (req, res) {
   const database = req.app.get('database');
 
   let messages;
-  for (let i = 0; i<5; i++) {
+  for (let i = 0; i<100; i++) {
     messages = await database.query('SELECT * FROM messages;').then(r => r.rows);
   }
 
